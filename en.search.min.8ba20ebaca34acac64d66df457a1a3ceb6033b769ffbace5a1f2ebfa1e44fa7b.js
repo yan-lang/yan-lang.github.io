0a7b6716312c07948eae113e9bf6e430b71d54ea
laizeqiang <916323301@qq.com>
2020-01-28 13:35:50 +0800

    rebuilding site Tue Jan 28 13:35:50 CST 2020

---
+++ en.search.min.8ba20ebaca34acac64d66df457a1a3ceb6033b769ffbace5a1f2ebfa1e44fa7b.js
@@ -0,0 +1,9 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(e){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();e.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.7e62e713e2e560d691b6dc1974d702fd1159194a6cf5a50f9307f64ba0da2dd7.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
